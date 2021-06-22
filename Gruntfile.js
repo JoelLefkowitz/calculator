501f172efc20d62e4212886a68175c941b629659
joellefkowitz <joellefkowitz@hotmail.com>
2021-06-22 19:12:04 +0100

    Add publish workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,20 @@
 const toExecs = (arr) => arr.map((i) => "exec:".concat(i));
 
+const dockerRun = (image, volume, entrypoint, args) => 
+  `docker run -i  --rm  -v ${volume} --entrypoint ${entrypoint} ${image} ${args}`
+
 module.exports = function (grunt) {
   grunt.initConfig({
     exec: {
       cspell: 'npx cspell ".*" "*" "**/*"',
       csscomb: "npx csscomb . -t -v",
-      dockerBuild: "docker build . -t joellefkowitz/calculator:prod",
-      dockerPush: "docker push joellefkowitz/calculator:prod",
       eslint: "npx eslint . --fix --ignore-path .gitignore",
       eslintWatch: "npx esw . -w --fix --ignore-path .gitignore",
       express: "node express/main.js",
       karma: "npx karma start app/karma.conf.js --single-run",
       karmaWatch: "npx karma start app/karma.conf.js",
       prettier: "npx prettier . --write --ignore-path .gitignore", 
-      testCafe: `
-        docker run -i --rm -v $(pwd):/cafe
-        --entrypoint node testcafe/testcafe
-        cafe/express/e2e.js
-        `,
+      testCafe: dockerRun("testcafe/testcafe", "$(pwd):/cafe", "node", "cafe/express/e2e.js"),
       testCafeLocal: 'node express/e2e.js',
       webpack: "npx webpack -c app/webpack.config.js",
       webpackWatch: "npx webpack -c app/webpack.config.js --watch",
@@ -47,11 +44,6 @@ module.exports = function (grunt) {
     toExecs(["webpack"])
   );
   grunt.registerTask(
-    "serve",
-    "Build a bundle and serve in watch mode",
-    "concurrent:serve"
-  );
-  grunt.registerTask(
     "test",
     "Sequentially run all unit test suites",
     toExecs(["karma"])
@@ -59,11 +51,11 @@ module.exports = function (grunt) {
   grunt.registerTask(
     "e2e",
     "Build a bundle and serve and sequentially run all e2e test suites",
-    ["exec:webpack", "exec:testCafe"]
+    toExecs(["webpack", "testCafe"])
   );
   grunt.registerTask(
-    "publish",
-    "Build and push an image",
-    toExecs(["dockerBuild", "dockerPush"])
+    "serve",
+    "Build a bundle and serve in watch mode",
+    "concurrent:serve"
   );
 };
